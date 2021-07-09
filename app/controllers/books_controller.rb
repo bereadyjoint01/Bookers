e1cb2ef35c4308b6bb01b8b9e4fb10afb141b581
bereadyjoint01 <bereadyjoint01@yahoo.co.jp>
2021-07-09 10:04:28 +0000

    [Bookers]1-3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,25 @@
 class BooksController < ApplicationController
-  
+  def show
+  end
+
+  def index
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to '/books'
+  end
+
+  def edit
+  end
+
+   private
+  def blog_params
+    params.permit(:title, :body)
+  end
 end
